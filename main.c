7cc355763a82f1086968a7a341ae1424090f8d35
devglen <devglen@icloud.com>
2015-03-14 13:35:15 -0400

    cleanup updates

--- main.c
+++ main.c
@@ -12,17 +12,17 @@ int main(void)
     while (1) {
         printf("flop: ");
         char cmd[50];
-		char argument[50];
+	char argument[50];
 		
-		scanf("%s", cmd);
-		if (strcmp(str(quit), cmd) == 0) {
-			printf("Exiting the floppy disk shell...");
-			return EXIT_SUCCESS;
-		} else if (strcmp(str(help), cmd) == 0) {
+	scanf("%s", cmd);
+	if (strcmp(str(quit), cmd) == 0) {
+		printf("Exiting the floppy disk shell...");
+		return EXIT_SUCCESS;
+	} else if (strcmp(str(help), cmd) == 0) {
             help(); 
         } else if (strcmp(str(fmount), cmd) == 0) {
             scanf("%s", argument);
-            fd = fmount(argument);
+            fmount(argument);
         } else if (strcmp(str(fumount), cmd) == 0) {
             fumount(fd);
         } else if (strcmp(str(structure), cmd) == 0) {
@@ -32,7 +32,7 @@ int main(void)
             traverse(argument);
         } else if (strcmp(str(showsector), cmd) == 0) {
             scanf("%s", argument);
-            show_sector(argument);
+            show_sector(atoi(argument));
         } else if (strcmp(str(showfat), cmd) == 0) {
             show_fat();
         } else {
