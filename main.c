666ccc7fb158c8c13b315d57bea5622c6aee0589
devglen <devglen@icloud.com>
2015-03-14 12:13:57 -0400

    added enum for readability

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "flop.h"
+
+#define str(x) #x
+
+enum Command { fmount, fumount, structure, traverse, showsector, showfat, quit, help };
 
 int main(void)
 {
@@ -13,25 +21,25 @@ int main(void)
         fgets(input, 20, stdin); //read line
         sscanf(input, "%s", cmd); //separate out first arg
 
-        if (strcmp(cmd, "help") == 0) {
+        if (strcmp(str(help), cmd) == 0) {
             help(); 
-        } else if (strcmp(cmd, "fmount") == 0) {
+        } else if (strcmp(str(fmount), cmd) == 0) {
             sscanf(input, "%s %s", cmd, img);
             fd = fmount(img);
-        } else if (strcmp(cmd, "fumount") == 0) {
+        } else if (strcmp(str(fumount), cmd) == 0) {
             fumount(fd);
-        } else if (strcmp(cmd, "structure") == 0) {
+        } else if (strcmp(str(structure), cmd) == 0) {
             structure(fd);
-        } else if (strcmp(cmd, "traverse") == 0) {
+        } else if (strcmp(str(traverse), cmd) == 0) {
             sscanf(input, "%s %s", cmd, flag);
             traverse(fd, flag[1]);
             flag[1] = ' '; //clear flag
-        } else if (strcmp(cmd, "showsector") == 0) {
+        } else if (strcmp(strcmp(str(showsector), cmd) == 0) {
             sscanf(input, "%s %d", cmd, &sector);
             show_sector(fd, sector);
-        } else if (strcmp(cmd, "showfat") == 0) {
+        } else if (strcmp(str(showfat), cmd) == 0) {
             show_fat(fd);
-        } else if (strcmp(cmd, "quit") == 0) {
+        } else if (strcmp(str(quit), cmd) == 0) {
             exit(0);
         } else {
             printf("Error: invalid command\n");
