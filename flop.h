0242d3de3c26c0c66cfb956e13c35f5751324df3
devglen <devglen@icloud.com>
2015-03-14 11:44:18 -0400

    variables used in show fat method

--- flop.h
+++ flop.h
@@ -1,6 +1,13 @@
 #ifndef __FLOP_H__
 #define __FLOP_H__
 
+int fd;
+unsigned short bytes_per_sector;
+unsigned short num_of_sectors;
+unsigned short fatbytes;
+unsigned short cluster;
+char *fat_buffer;
+
 void help(void);
 int fmount(char *);
 void fumount(int);
