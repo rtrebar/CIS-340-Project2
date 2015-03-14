8054f010f9fe114281caa1b7c4c57cd1a29c7d28
devglen <devglen@icloud.com>
2015-03-14 12:16:20 -0400

    removing duplicate removes

--- main.c
+++ main.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
 #include <errno.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include "flop.h"
 
 #define str(x) #x
