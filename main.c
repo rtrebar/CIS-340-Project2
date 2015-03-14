251464a639b076b048efd3919c7bac62dbc0308b
devglen <devglen@icloud.com>
2015-03-14 13:24:44 -0400

    updating project main to compile and work properly

--- main.c
+++ main.c
@@ -8,33 +8,33 @@ enum Command { fmount, fumount, structure, traverse, showsector, showfat, quit, 
 
 int main(void)
 {
-    char input[100], img[50], cmd[20], flag[2];
-
+    
     while (1) {
         printf("flop: ");
-        fgets(input, 20, stdin); //read line
-        sscanf(input, "%s", cmd); //separate out first arg
-
-        if (strcmp(str(help), cmd) == 0) {
+        char cmd[50];
+		char argument[50];
+		
+		scanf("%s", cmd);
+		if (strcmp(str(quit), cmd) == 0) {
+			printf("Exiting the floppy disk shell...");
+			return EXIT_SUCCESS;
+		} else if (strcmp(str(help), cmd) == 0) {
             help(); 
         } else if (strcmp(str(fmount), cmd) == 0) {
-            sscanf(input, "%s %s", cmd, img);
-            fd = fmount(img);
+            scanf("%s", argument);
+            fd = fmount(argument);
         } else if (strcmp(str(fumount), cmd) == 0) {
             fumount(fd);
         } else if (strcmp(str(structure), cmd) == 0) {
-            structure(fd);
+            structure();
         } else if (strcmp(str(traverse), cmd) == 0) {
-            sscanf(input, "%s %s", cmd, flag);
-            traverse(fd, flag[1]);
-            flag[1] = ' '; //clear flag
+            scanf("%s", argument);
+            traverse(argument);
         } else if (strcmp(str(showsector), cmd) == 0) {
-            sscanf(input, "%s %d", cmd, &sector);
-            show_sector(sector);
+            scanf("%s", argument);
+            show_sector(argument);
         } else if (strcmp(str(showfat), cmd) == 0) {
-            show_fat(fd);
-        } else if (strcmp(str(quit), cmd) == 0) {
-            exit(0);
+            show_fat();
         } else {
             printf("Error: invalid command\n");
         }
