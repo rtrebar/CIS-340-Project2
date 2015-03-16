0dce7031d1c720c44714ff219bd22bf79e4ca1ca
devglen <devglen@icloud.com>
2015-03-15 20:20:02 -0400

    making all methods printing output compatible with redirection

--- flop.c
+++ flop.c
@@ -111,7 +111,7 @@ void show_sector(int sec){
 	printf("\n");
 }
 
-int show_fat()
+void show_fat()
 {
     	unsigned short low, high;
 	char buf[32];
@@ -186,5 +186,4 @@ int show_fat()
 
 	printf("\n");
 	free(fat_buffer);
-	return 0;
 }
