4824d4aaf04318eeca1e86998b527d9691e6b980
Oisota <dman6505@gmail.com>
2015-03-20 15:05:13 -0400

    improved readability in help function

--- flop.c
+++ flop.c
@@ -9,14 +9,14 @@
 void help(void)
 {
     printf("The available commands are:\n");
-    printf("\t help \t - display a list of available commands.\n");
-    printf("\t fmount file_name \t - mount the specified image file\n");
-    printf("\t fumount \t - umount the mounted floppy disk. \n");
-    printf("\t structure \t - list the structure of the floppy disk image.\n");
-    printf("\t traverse [-l] \t - list the contents in the root directory. Optional -l flag gives a long listing of the root directory.\n");
-    printf("\t showsector [sector_num] \t - show the content of the given sector.\n");
-    printf("\t showfat \t - show the content of the FAT table.\n");
-    printf("\t quit \t - quit the floppy shell \n");
+    printf("    help                    - display a list of available commands.\n");
+    printf("    fmount <file_name>      - mount the specified image file\n");
+    printf("    fumount                 - umount the mounted floppy disk. \n");
+    printf("    structure               - list the structure of the floppy disk image.\n");
+    printf("    traverse [-l]           - list the contents in the root directory. Optional -l flag gives a long listing of the root directory.\n");
+    printf("    showsector <sector_num> - show the content of the given sector.\n");
+    printf("    showfat                 - show the content of the FAT table.\n");
+    printf("    quit                    - quit the floppy shell \n");
 }
 
 void fmount(const char* filename)
