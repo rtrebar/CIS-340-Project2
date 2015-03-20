5caaa7a1e03d2add171d90ab8609fd052f0cd512
Oisota <dman6505@gmail.com>
2015-03-20 14:43:34 -0400

    reformatted code

--- main.c
+++ main.c
@@ -18,22 +18,21 @@ int main(void)
 
         fgets(input, 50, stdin); //read line
         sscanf(input, "%s", command); //separate out first arg, consider using strtok() instead of sscanf()
+
         if (strcmp("quit", command) == 0) {
             printf("\n Exiting the floppy disk shell... \n");
             return EXIT_SUCCESS;
         } else if (strcmp("help", command) == 0) {
             if (strchr(input, '>') == NULL) {
                 help();
-            }
-            else {
+            } else {
                 sscanf(input, "%s %s %s", command, redir_symbol, redir_filename);
                 char* comd[] = {command, NULL, redir_filename};
                 if (strchr(redir_symbol, '>') != NULL) {
                     void (*func);
                     func = &help;
                     output_redirection(new_fd, redir_symbol, comd, func);
-                }
-                else {
+                } else {
                     printf("invalid commmand, please try again.\n");
                 }
             }
@@ -45,16 +44,14 @@ int main(void)
         } else if (strcmp("structure", command) == 0) {
             if (strchr(input, '>') == NULL) {
                 structure();
-            }
-            else {
+            } else {
                 sscanf(input, "%s %s %s", command, redir_symbol, redir_filename);
                 char* comd[] = {command, NULL, redir_filename};
                 if (strchr(redir_symbol, '>') != NULL) {
                     void (*func);
                     func = &structure;
                     output_redirection(new_fd, redir_symbol, comd, func);
-                }
-                else {
+                } else {
                     printf("invalid commmand, please try again.\n");
                 }
             }
@@ -62,16 +59,13 @@ int main(void)
             if (strchr(input, '>') == NULL) {
                 if (strchr(input, list_arg) == NULL) {
                     traverse(command);
-                }
-                else if (strchr(input, list_arg) != NULL) {
+                } else if (strchr(input, list_arg) != NULL) {
                     sscanf(input, "%s %s", command, arg);
                     traverse(arg);
-                }
-                else {
+                } else {
                     printf("Error invalid argument, please try again! \n");
                 }
-            }
-            else {
+            } else {
                 if (strchr(input, list_arg) == NULL) {
                     sscanf(input, "%s %s %s", command, redir_symbol, redir_filename);
                     char* comd[] = {command, redir_symbol, redir_filename};
@@ -79,24 +73,20 @@ int main(void)
                         void (*func)(char*);
                         func = &traverse;
                         output_redirection2(new_fd, redir_symbol, comd, func);
-                    }
-                    else {
+                    } else {
                         printf("invalid commmand, please try again.\n");
                     }
-                }
-                else if (strchr(input, list_arg) != NULL) {
+                } else if (strchr(input, list_arg) != NULL) {
                     sscanf(input, "%s %s %s %s", command, arg, redir_symbol, redir_filename);
                     char* comd[] = {command, arg, redir_filename};
                     if (strchr(redir_symbol, '>') != NULL) {
                         void (*func)(char*);
                         func = &traverse;
                         output_redirection2(new_fd, redir_symbol, comd, func);
-                    }
-                    else {
+                    } else {
                         printf("invalid commmand, please try again.\n");
                     }
-                }
-                else {
+                } else {
                     printf("Error invalid argument, please try again! \n");
                 }
             }
@@ -104,32 +94,28 @@ int main(void)
             if (strchr(input, '>') == NULL) {
                 sscanf(input, "%s %s", command, arg);
                 show_sector(atoi(arg));
-            }
-            else {
+            } else {
                 sscanf(input, "%s %s %s %s", command, arg, redir_symbol, redir_filename);
                 char* comd[] = {command, arg, redir_filename};
                 if (strchr(redir_symbol, '>') != NULL) {
                     void (*func)(int);
                     func = &show_sector;
                     output_redirection3(new_fd, redir_symbol, comd, func);
-                }
-                else {
+                } else {
                     printf("invalid commmand, please try again.\n");
                 }
             }
         } else if (strcmp("showfat", command) == 0) {
             if (strchr(input, '>') == NULL) {
                 show_fat();
-            }
-            else {
+            } else {
                 sscanf(input, "%s %s %s", command, redir_symbol, redir_filename);
                 char* comd[] = {command, NULL, redir_filename};
                 if (strchr(redir_symbol, '>') != NULL) {
                     void (*func);
                     func = &show_fat;
                     output_redirection(new_fd, redir_symbol, comd, func);
-                }
-                else {
+                } else {
                     printf("invalid commmand, please try again.\n");
                 }
             }
